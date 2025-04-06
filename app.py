7ccfd93494cae0ed75c10b1b5ab0d64712bddc62
theyousefsaad <yousefwalidsaad2-14@gmail.com>
2025-04-06 18:45:37 -0500

    Fix: bind Flask app to Render port

--- app.py
+++ app.py
@@ -49,3 +49,9 @@ def upload():
 
     else:
         return jsonify({"error": "Unsupported Media Type"}), 415
+
+        if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))  # default to 10000 locally
+    app.run(host='0.0.0.0', port=port)
+
