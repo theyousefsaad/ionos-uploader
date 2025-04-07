20bd86e2315b309a51c03b4d0b59ebc205159cb3
theyousefsaad <yousefwalidsaad2-14@gmail.com>
2025-04-06 19:41:28 -0500

    Enable debug mode

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def upload():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
     app.run(host="0.0.0.0", port=port)
